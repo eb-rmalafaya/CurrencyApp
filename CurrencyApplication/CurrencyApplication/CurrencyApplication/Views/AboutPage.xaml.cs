bd197fcd0e2366bb60ad43167e2b0dad80cbaaed
Paulo Barbosa[1130648] <1130648@isep.ipp.pt>
2017-04-27 17:53:15 +0100

    base project (lower windows version and master project)

---
+++ CurrencyApplication/CurrencyApplication/CurrencyApplication/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace CurrencyApplication.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ CurrencyApplication/CurrencyApplication/CurrencyApplication/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using CurrencyApplication.ViewModels;
+
+using Xamarin.Forms;
+
+namespace CurrencyApplication.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
