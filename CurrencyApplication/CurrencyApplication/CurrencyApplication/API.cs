1883843619c98345e5bbff8196e41a3754be5321
Paulo Barbosa[1130648] <1130648@isep.ipp.pt>
2017-04-30 17:30:07 +0100

    add. rest service with JSON library

---
+++ CurrencyApplication/CurrencyApplication/CurrencyApplication/API.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyApplication
+{
+    class API
+    {
+        // URL of REST service 
+        public static string RestUrl = "http://YAHOOURL";
+        // Credentials that are hard coded into the REST service
+        public static string Username = "Xamarin";
+        public static string Password = "Pa$$w0rd";
+    }
+}
---
+++ CurrencyApplication/CurrencyApplication/CurrencyApplication/Helpers/IHTTPhandler.cs
@@ -0,0 +1,18 @@
+﻿using CurrencyApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyApplication.Helpers
+{
+    interface IHTTPhandler
+    {
+        Task<List<CurrencyDTO>> RefreshDataAsync();
+
+        Task SaveTodoItemAsync(CurrencyDTO item, bool isNewItem);
+
+        Task DeleteTodoItemAsync(string id);
+    }
+}
