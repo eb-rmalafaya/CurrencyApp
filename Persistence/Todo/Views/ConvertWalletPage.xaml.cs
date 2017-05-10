ffd632d5e5aedd744d43b32146e37d78f87eab6f
Paulo Barbosa[1130648] <1130648@isep.ipp.pt>
2017-05-10 16:00:08 +0100

    edit. ConvertWallet page moved ui elements from xaml to class

--- Persistence/Todo/Views/ConvertWalletPage.xaml.cs
+++ Persistence/Todo/Views/ConvertWalletPage.xaml.cs
@@ -3,9 +3,6 @@ using CurrencyApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,14 +12,58 @@ namespace CurrencyApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConvertWalletPage : ContentPage
     {
+        private Picker Currency;
+        private Button Button;
+
         public ConvertWalletPage()
         {
-            InitializeComponent();
+            //InitializeComponent();
+
+            Currency = new Picker { Title = "Choose" };
             foreach (String symbol in CurrencyDTO.top10Currencies)
             {
                 if (Currency.SelectedIndex != 0) Currency.SelectedIndex = 0;
                 Currency.Items.Add(symbol);
             }
+                     
+            //
+            Button button = new Button { Text = "Convert" };
+            button.Clicked += delegate
+            {
+                OnConvert();
+            };
+
+            var contentView = new ContentView
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.FillAndExpand,
+                    Children = {
+                     Currency,
+                     button,
+                 }
+                }
+            };
+            Content = contentView;
+        }
+
+        async void OnConvert()
+        {
+            try
+            {
+                string toSymbol = (string)Currency.SelectedItem;
+                //Get Wallets
+                List<Wallet> wallets = await App.Database.GetItemsAsync();
+                foreach (Wallet wallet in wallets)
+                {
+                    await APIHandler.Convert(wallet.Symbol, toSymbol, wallet, wallet.Quantity);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+            }
+            await Navigation.PopToRootAsync();
         }
 
         async void OnConvert(object sender, EventArgs e)
